d5259c530f5b2208a51a986e9c7cbbac26012d67
@mr.klava <80220184+mrKlava@users.noreply.github.com>
2023-05-16 12:50:05 +0000

    added deque on python

--- test.py
+++ test.py
@@ -1,103 +1,39 @@
-'''Max-heap a.k.a binary heap implementation'''
+'''Deque using list'''
 
-array = []    # declare empty array for structure
-
-
-def heapify(arr, n, i):
-    '''
-    Create binary heap from array
-
-    arr -> array used to store binary heap
-    n   -> size of array
-    i   -> index of item
-    '''
-    largest = i     # current index is largest
-
-    l = 2 * i + 1   # left node
-    r = 2 * i + 2   # right node
-
-    if l < n and arr[i] < arr[l]:   # if left node > largest -> swap
-        largest = l
-
-    if r < n and arr[i] < arr[r]:   # if right node > largest -> swap
-        largest = r
-
-    if largest != i:    # if found new largest -> swap nodes & heapify again
-        arr[i], arr[largest] = arr[largest], arr[i]
-
-        heapify(arr, n, largest)
-
-
-def insert(arr, num):
-    '''
-    Insert new item to binary heap
+class Deque:
+    def __init__(self):
+        self.deque = []
     
-    arr -> array used to store binary heap
-    num -> new item :int
-    '''
-    size = len(arr)     # get len of current binary heap
-
-    if size == 0:       # if binary heap is empty -> push new item
-        arr.append(num)
-    else:               # if not -> push new item and heapify all structure
-        arr.append(num)
-            # (size//2) - 1 => it is middle of array; range(start, stop, step)
-        for i in range((size//2) - 1, -1, -1):
-            heapify(arr, size, i)
-
-
-def delete(arr, num):
-    '''
-    Remove item from binary heap
+    def is_empty(self):
+        return self.deque == []
     
-    arr -> array used to store binary heap
-    num -> item :int
-    '''
-    size = len(arr)             # get len of current tree
-    i = 0                       # init index (current element, starting from first)
+    def enqueue_rear(self, item):
+        self.deque.append(item)
 
-    for i in range(0, size):    # use linear search to find index of item
-        if num == arr[i]:       # ?? IF NOT FOUND THAN WHAT ??
-            break
+    def enqueue_front(self, item):
+        self.deque.insert(0, item)
     
-    arr[i], arr[size - 1] = arr[size - 1], arr[i]   # swap last node with target
-    arr.remove(num)     # delete target node
+    def dequeue_rear(self):
+        return self.deque.pop()
 
-    size = len(arr)     # redefine new tree length
+    def dequeue_front(self):
+        return self.deque.pop(0)
+    
+    def size(self):
+        return len(self.deque)
+    
 
-    for i in range((size//2) - 1, -1, -1):      # heapify tree from deleted element
-        heapify(arr, size, i)
-
-
-# test = [3, 9, 2, 1, 4, 5]
-
-# print(test)
-
-# heapify(test, len(test), 0)
-
-# print(test)
-
-# insert(test, 7)
-
-# print(test)
-
-
-insert(array, 3)
-insert(array, 9)
-insert(array, 2)
-insert(array, 1)
-insert(array, 4)
-insert(array, 5)
-insert(array, 10)
-insert(array, 12)
-
-print ("Max-Heap array: " + str(array))
-
-
-# insert(array, 3)
-# insert(array, 4)
-# insert(array, 9)
-# insert(array, 5)
-# insert(array, 2)
-
-# print ("Max-Heap array: " + str(array))
+d = Deque()
+print(d.is_empty())
+d.enqueue_rear(8)
+d.enqueue_rear(5)
+d.enqueue_front(7)
+d.enqueue_front(10)
+print(d.size())
+print(d.is_empty())
+d.enqueue_rear(11)
+print(d.dequeue_rear())
+print(d.dequeue_front())
+d.enqueue_front(55)
+d.enqueue_rear(45)
+print(d.deque)
