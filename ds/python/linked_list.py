5d2aefef35a0f09425322c0dcfd30d28545370a1
@mr.klava <80220184+mrKlava@users.noreply.github.com>
2023-07-07 13:48:03 +0000

    added standart functinality of linked list fot python

--- ds/python/linked_list.py
+++ ds/python/linked_list.py
@@ -18,16 +18,50 @@ class LinkedList:
         '''access linked list elements'''
         temp = self.head
 
+        print('-----Linked List start')
+
         while temp is not None:
-            print(temp.data, end='\n')
+            print(temp.data)
 
             if temp.next is not None:
                 temp = temp.next
             else:
                 break
+        print('-----Linked List end \n')
 
 
-    def insert_beginning(self, node):
+    def search(self, value):
+        '''returns true if linked list includes value'''
+        temp = self.head
+
+        while temp is not None:
+            if temp.data == value:
+                return True
+            temp = temp.next
+
+        return False
+
+
+    def sort(self):
+        '''sort linked list'''
+        temp = self.head
+        index = Node(None)
+
+        if temp is None:
+            return
+        else:
+            while temp is not None:
+                index = temp.next
+
+                while index is not None:
+                    if temp.data > index.data:
+                        temp.data, index.data = index.data, temp.data
+
+                    index = index.next
+                temp = temp.next
+
+
+    def insert_beg(self, node):
         '''insert node as a head'''
         node.next = self.head
         self.head = node
@@ -37,29 +71,65 @@ class LinkedList:
         '''insert node to the end of list'''
         temp = self.head
 
+        if temp is None:
+            self.head = node
+
         while temp.next is not None:
             temp = temp.next
 
         temp.next = node
 
 
-    def insert_at(self, node, pos):
+    def insert_at(self, pos, node):
         '''insert note at position'''
         temp = self.head
 
-        for i in range(pos):
+        if pos == 0:
+            self.insert_beg(node)
+            return
 
-            if temp is None:
+        for i in range(pos - 1):
+
+            if temp.next is None:
                 print('error. List is to short', end='\n')
                 return
+
             temp = temp.next
-            print(temp.data)
 
-        rear = temp
-        node.next = rear
-        temp = node
-        
+        node.next = temp.next
+        temp.next = node
 
+
+    def delete_beg(self):
+        '''delete first node'''
+        self.head = self.head.next
+
+
+    def delete_end(self):
+        '''delete last node'''
+        temp = self.head
+
+        while temp.next.next is not None:
+            temp = temp.next
+
+        temp.next = None
+
+
+    def delete_at(self, pos):
+        '''delete node at index'''
+        temp = self.head
+
+        if pos == 0:
+            self.delete_beg()
+            return
+
+        for i in range(pos - 1):
+            if temp.next.next is None:
+                print('error. list is to short')
+                return
+            temp = temp.next
+
+        temp.next = temp.next.next
 
 # create empty linked list
 linked_list = LinkedList()
@@ -79,13 +149,21 @@ second.next = third
 # set linked list header
 linked_list.head = first
 
-
-linked_list.insert_beginning(zero)
+linked_list.insert_beg(zero)
 linked_list.insert_end(fourth)
-linked_list.insert_at(fifth, 3)
+linked_list.insert_at(2, fifth)
 
+# linked_list.delete_beg()
+# linked_list.delete_end()
+# linked_list.delete_at(3)
 
-# linked_list.traverse()
+linked_list.traverse()
+
+print(linked_list.search(5))
+
+linked_list.sort()
+
+linked_list.traverse()
 
 
 # # print out linked list
